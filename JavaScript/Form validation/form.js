10c4d0e2fe0270c5b2e9cfd123105f5d36efd8d1
mm lab 40 <aravindbusiness7@gmail.com>
2026-06-02 16:23:10 +0530

    final

--- JavaScript/Form validation/form.js
+++ JavaScript/Form validation/form.js
@@ -1,19 +1,40 @@
-const namebox = document.getElementById("text-box")
-const age = document.getElementById("age-box")
-const email = document.getElementById("email-box")
-const number = document.getElementById("number-box")
-const submitbtn = document.getElementById("submit-btn")
+const namebox = document.getElementById("text-box");
+const age = document.getElementById("age-box");
+const email = document.getElementById("email-box");
+const number = document.getElementById("number-box");
+const submitbtn = document.getElementById("submit-btn");
+const error = document.getElementById("error-text");
 
-submitbtn.addEventListener("click",Validation)
+submitbtn.addEventListener("click", Validation);
 
-function Validation(){
-    
-   
-    // validation goes here
+function Validation(e) {
+  e.preventDefault();
 
-    if(namebox.value === " " || "/^[A-Za-Z]+$/ ") {
-        
-    }
+        //Name Validation Goes Here
+
+  if (namebox.value === "") { 
+
+    error.innerHTML = "Empty Fields Not Allowed";
+
+    setTimeout(() => {
+      error.innerHTML = "";
+    }, 2000);
+
+
+  } else if (!/^[A-Za-z]+$/.test(namebox.value)) {
+
+    error.innerHTML = "Numbers are Not Allowed";
+
+    setTimeout(() => {
+      error.innerHTML = "";
+    }, 2000);
+  }
+
 }
 
 
+//age validation 
+
+if(!age === (0-9)){
+   error.innerHTML = "Enter Valid Number"
+}
